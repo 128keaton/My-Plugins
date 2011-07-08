9e19a71fb7a9c73933b883e41c3d937846bc4973
Nick Khoury <nick.khry@gmail.com>
2011-07-08 10:44:50 -0400

    Readded configuration file (working)

--- BroadcastDonator/src/xrevolut1onzx/broadcastdonator/BroadcastDonator.java
+++ BroadcastDonator/src/xrevolut1onzx/broadcastdonator/BroadcastDonator.java
@@ -1,7 +1,14 @@
 package xrevolut1onzx.broadcastdonator;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.logging.Logger;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -13,16 +20,23 @@ import com.nijikokun.bukkit.Permissions.Permissions;
 
 public class BroadcastDonator extends JavaPlugin {
 
+	// Configuration variables
+	public static String mainDirectory = "plugins/BroadcastDonator";
+	public static File config = new File(mainDirectory + File.separator
+			+ "config.yml");
+	public static Properties prop = new Properties();
+
 	/**
 	 * String that is used to contain the message that is broadcast throughout
 	 * the server
 	 */
-	public String messageToSend = "Test";
+	public String messageToSend;
 
 	/**
 	 * Declares the logger. The logger allows you to write information to the
 	 * console and to the server.log file
 	 */
+	public static final String logPrefix = "[BD] ";
 	Logger log = Logger.getLogger("Minecraft");
 
 	// Declaration for permissions support
@@ -44,7 +58,8 @@ public class BroadcastDonator extends JavaPlugin {
 			if (messageToSend != null) {
 				getServer().broadcastMessage(messageToSend);
 			} else {
-				log.info("You didn't set the message to broadcast!");
+				commandTyper.sendMessage(ChatColor.DARK_RED + "Reload the configuration file to load your message!");
+				log("Reload the configuration file to load your message!");
 			}
 			return true;
 		} // If this has happened the function will break and return true. if
@@ -54,15 +69,57 @@ public class BroadcastDonator extends JavaPlugin {
 
 	// Called on a clean stop of the server
 	public void onDisable() {
-
+		log("Disabled");
 	}
 
 	// Called on server start
 	public void onEnable() {
-		log.info("[BD] Initiating plugin...");
+		log("Initiating plugin...");
+		// Handles the configuration file
+		manageConfigFile();
 		// Sets up permissions
 		setupPermissions();
-		log.info("[BD] Initialized");
+		log("Initialized");
+	}
+
+	// Manages the configuration file
+	public void manageConfigFile() {
+		new File(mainDirectory).mkdir(); // Creates the plugin's folder
+		if (!config.exists()) {
+			try {
+				config.createNewFile();
+				FileOutputStream output = new FileOutputStream(config);
+				prop.put(
+						"MessageToBroadcast",
+						"[Server] Enjoy this server? Consider donating to help fund it! Options available on our website.");
+				prop.store(output, "Edit the configurations to your liking");
+				output.flush();
+				output.close();
+				log("Configuration file created. Please go update and reload it.");
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		} else {
+			loadConfigFile();
+			log("Configuration file loaded");
+		}
+	}
+
+	public void loadConfigFile() {
+		try {
+			FileInputStream input = new FileInputStream(config);
+			prop.load(input);
+			messageToSend = prop.getProperty("MessageToBroadcast");
+			input.close();
+		} catch (FileNotFoundException ex) {
+			ex.printStackTrace();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public void log (String message) {
+		log.info(logPrefix + message);
 	}
 
 	// Permissions setup method. Called only at server start
@@ -75,12 +132,12 @@ public class BroadcastDonator extends JavaPlugin {
 				.getPlugin("Permissions");
 
 		if (permissionsPlugin == null) {
-			log.info("[BD] Permission system not detected, defaulting to OP");
+			log("Permission system not detected, defaulting to OP");
 			return;
 		}
 
 		permissionHandler = ((Permissions) permissionsPlugin).getHandler();
-		log.info("[BD] Permissions found, using permissions instead of OP");
+		log("Permissions found, using permissions instead of OP");
 	}
 
 }
