7c3ca9a05dc5f8f713ceca54cfa4d645d8379c76
Nick Khoury <nick.khry@gmail.com>
2011-07-29 11:43:44 -0500

    Readded Permissions and OP support with options in the configuration file

--- BroadcastDonator/src/xrevolut1onzx/broadcastdonator/BroadcastDonator.java
+++ BroadcastDonator/src/xrevolut1onzx/broadcastdonator/BroadcastDonator.java
@@ -11,7 +11,11 @@ import java.util.logging.Logger;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.nijiko.permissions.PermissionHandler;
+import com.nijikokun.bukkit.Permissions.Permissions;
 
 public class BroadcastDonator extends JavaPlugin {
 
@@ -20,6 +24,15 @@ public class BroadcastDonator extends JavaPlugin {
 	public static File config = new File(mainDirectory + File.separator
 			+ "config.yml");
 	public static Properties prop = new Properties();
+
+	/**
+	 * Permission varaiblas
+	 */
+	public static PermissionHandler permissionHandler;
+	public static String permissionType = null;
+	public static boolean usingOp = false;
+	public static boolean usingPermissions = false;
+	public static boolean usingSuperPerms = false;
 
 	/**
 	 * String that is used to contain the message that is broadcast throughout
@@ -82,6 +95,7 @@ public class BroadcastDonator extends JavaPlugin {
 		log("Reloading...");
 		getServer().getScheduler().cancelAllTasks();
 		manageConfigFile();
+		checkPermissionType();
 		log("Reloaded");
 		handleRecurringMessage();
 	}
@@ -89,10 +103,44 @@ public class BroadcastDonator extends JavaPlugin {
 	// Called on server start
 	public void onEnable() {
 		log("Initiating plugin...");
-		// Handles the configuration file
 		manageConfigFile();
+		checkPermissionType();
 		log("Initialized");
 		handleRecurringMessage();
+	}
+
+	public void checkPermissionType() {
+		usingOp = false;
+		usingPermissions = false;
+		usingSuperPerms = false;
+		if (permissionType == null) {
+			log("Please reload the plugin to start using it");
+			return;
+		}
+		if (permissionType.equalsIgnoreCase("Permissions")) {
+			usingPermissions = true;
+			setupPermissions();
+		} else if (permissionType.equalsIgnoreCase("OP")) {
+			usingOp = true;
+		} else if (permissionType.equalsIgnoreCase("SuperPerms")) {
+			usingSuperPerms = true;
+		}
+	}
+
+	public void setupPermissions() {
+		if (permissionHandler != null) {
+			return;
+		}
+
+		Plugin permissionsPlugin = this.getServer().getPluginManager()
+				.getPlugin("Permissions");
+
+		if (permissionsPlugin == null) {
+			log("Permissions not detected, plugin disabled.");
+			return;
+		}
+
+		permissionHandler = ((Permissions) permissionsPlugin).getHandler();
 	}
 
 	public void handleRecurringMessage() {
@@ -109,9 +157,19 @@ public class BroadcastDonator extends JavaPlugin {
 										.replaceAll("&([0-9a-f])", "\u00A7$1"));
 								for (Player player : getServer()
 										.getOnlinePlayers()) {
-									if (!player
-											.hasPermission("broadcastdonator.exemptfrommessage")) {
-										player.sendMessage(finalMessage);
+									if (usingSuperPerms) {
+										if (!player
+												.hasPermission("broadcastdonator.exemptfrommessage")) {
+											player.sendMessage(finalMessage);
+										}
+									} else if (usingPermissions) {
+										if (!BroadcastDonator.permissionHandler.has(player, "broadcastdonator.exemptfrommessage")) {
+											player.sendMessage(finalMessage);
+										}
+									} else if (usingOp) {
+										if (!player.isOp()) {
+											player.sendMessage(finalMessage);
+										}
 									}
 								}
 								log(finalMessage);
@@ -131,8 +189,9 @@ public class BroadcastDonator extends JavaPlugin {
 			try {
 				config.createNewFile();
 				FileOutputStream output = new FileOutputStream(config);
+				prop.put("Permission-Manager", "Permissions");
 				prop.put(
-						"MessageToBroadcast",
+						"Message-To-Broadcast",
 						"[Server] Enjoy this server? Consider donating to help fund it! Options available on our website.");
 				prop.put("Recurring-Broadcast", "false");
 				prop.put("Time-between-messages-in-minutes", "30");
@@ -145,7 +204,6 @@ public class BroadcastDonator extends JavaPlugin {
 			}
 		} else {
 			loadConfigFile();
-			log("Configuration file loaded");
 		}
 	}
 
@@ -153,11 +211,12 @@ public class BroadcastDonator extends JavaPlugin {
 		try {
 			FileInputStream input = new FileInputStream(config);
 			prop.load(input);
-			rawMessage = prop.getProperty("MessageToBroadcast");
+			permissionType = prop.getProperty("Permission-Manager");
+			rawMessage = prop.getProperty("Message-To-Broadcast");
 			recurringMessage = Boolean.parseBoolean(prop
 					.getProperty("Recurring-Broadcast"));
 			timeDelay = Integer.parseInt(prop
-					.getProperty("Time-between-messages-in-minutes"));
+					.getProperty("Time-between-messages-in-minutes)"));
 			input.close();
 		} catch (FileNotFoundException ex) {
 			ex.printStackTrace();
--- BroadcastDonator/src/xrevolut1onzx/broadcastdonator/CommandsHandler.java
+++ BroadcastDonator/src/xrevolut1onzx/broadcastdonator/CommandsHandler.java
@@ -15,56 +15,170 @@ public class CommandsHandler {
 	}
 
 	public void preview(Player commandTyper) {
-		if (commandTyper.hasPermission("broadcastdonator.use")) {
-			if (plugin.rawMessage != null) {
-				String finalMessage = new String(plugin.rawMessage.replaceAll(
-						"&([0-9a-f])", "\u00A7$1"));
-				commandTyper.sendMessage(ChatColor.DARK_RED + "Preview: "
-						+ ChatColor.WHITE + finalMessage);
+		if (BroadcastDonator.usingSuperPerms) {
+			if (commandTyper.hasPermission("broadcastdonator.use")) {
+				if (plugin.rawMessage != null) {
+					String finalMessage = new String(
+							plugin.rawMessage.replaceAll("&([0-9a-f])",
+									"\u00A7$1"));
+					commandTyper.sendMessage(ChatColor.DARK_RED + "Preview: "
+							+ ChatColor.WHITE + finalMessage);
+				} else {
+					commandTyper
+							.sendMessage(ChatColor.DARK_RED
+									+ "Reload the configuration file to load your message!");
+					plugin.log("Reload the configuration file to load your message!");
+				}
 			} else {
 				commandTyper
-						.sendMessage(ChatColor.DARK_RED
-								+ "Reload the configuration file to load your message!");
-				plugin.log("Reload the configuration file to load your message!");
+						.sendMessage("You don't have permission to use this command.");
 			}
-		} else {
-			commandTyper
-					.sendMessage("You don't have permission to use this command.");
+		} else if (BroadcastDonator.usingPermissions) {
+			if (BroadcastDonator.permissionHandler.has(commandTyper,
+					"broadcastdonator.use")) {
+				if (plugin.rawMessage != null) {
+					String finalMessage = new String(
+							plugin.rawMessage.replaceAll("&([0-9a-f])",
+									"\u00A7$1"));
+					commandTyper.sendMessage(ChatColor.DARK_RED + "Preview: "
+							+ ChatColor.WHITE + finalMessage);
+				} else {
+					commandTyper
+							.sendMessage(ChatColor.DARK_RED
+									+ "Reload the configuration file to load your message!");
+					plugin.log("Reload the configuration file to load your message!");
+				}
+			} else {
+				commandTyper
+						.sendMessage("You don't have permission to use this command.");
+			}
+		} else if (BroadcastDonator.usingOp) {
+			if (commandTyper.isOp()) {
+				if (plugin.rawMessage != null) {
+					String finalMessage = new String(
+							plugin.rawMessage.replaceAll("&([0-9a-f])",
+									"\u00A7$1"));
+					commandTyper.sendMessage(ChatColor.DARK_RED + "Preview: "
+							+ ChatColor.WHITE + finalMessage);
+				} else {
+					commandTyper
+							.sendMessage(ChatColor.DARK_RED
+									+ "Reload the configuration file to load your message!");
+					plugin.log("Reload the configuration file to load your message!");
+				}
+			} else {
+				commandTyper
+						.sendMessage("You don't have permission to use this command.");
+			}
 		}
 	}
 
 	public void reload(Player commandTyper) {
-		if (commandTyper.hasPermission("broadcastdonator.reload")) {
-			plugin.onReload();
-			plugin.log("Plugin reloaded by " + commandTyper.getName());
-			commandTyper.sendMessage(ChatColor.GRAY + "Plugin reloaded");
-		} else {
-			commandTyper
-					.sendMessage("You don't have permission to use this command.");
+		if (BroadcastDonator.usingSuperPerms) {
+			if (commandTyper.hasPermission("broadcastdonator.reload")) {
+				plugin.onReload();
+				plugin.log("Plugin reloaded by " + commandTyper.getName());
+				commandTyper.sendMessage(ChatColor.GRAY + "Plugin reloaded");
+			} else {
+				commandTyper
+						.sendMessage("You don't have permission to use this command.");
+			}
+		} else if (BroadcastDonator.usingPermissions) {
+			if (BroadcastDonator.permissionHandler.has(commandTyper,
+					"broacastdonator.reload")) {
+				plugin.onReload();
+				plugin.log("Plugin reloaded by " + commandTyper.getName());
+				commandTyper.sendMessage(ChatColor.GRAY + "Plugin reloaded");
+			} else {
+				commandTyper
+						.sendMessage("You don't have permission to use this command.");
+			}
+		} else if (BroadcastDonator.usingOp) {
+			if (BroadcastDonator.usingOp) {
+				plugin.onReload();
+				plugin.log("Plugin reloaded by " + commandTyper.getName());
+				commandTyper.sendMessage(ChatColor.GRAY + "Plugin reloaded");
+			} else {
+				commandTyper
+						.sendMessage("You don't have permission to use this command.");
+			}
 		}
 	}
 
 	public void broadcast(Player commandTyper) {
-		if (commandTyper.hasPermission("broadcastdonator.use")) {
-			if (plugin.rawMessage != null) {
-				String finalMessage = new String(plugin.rawMessage.replaceAll(
-						"&([0-9a-f])", "\u00A7$1"));
-				for (Player player : plugin.getServer().getOnlinePlayers()) {
-					if (!player.hasPermission("broadcastdonator.exemptfrommessage")) {
-						player.sendMessage(finalMessage);
+		if (BroadcastDonator.usingSuperPerms) {
+			if (commandTyper.hasPermission("broadcastdonator.use")) {
+				if (plugin.rawMessage != null) {
+					String finalMessage = new String(
+							plugin.rawMessage.replaceAll("&([0-9a-f])",
+									"\u00A7$1"));
+					for (Player player : plugin.getServer().getOnlinePlayers()) {
+						if (!player
+								.hasPermission("broadcastdonator.exemptfrommessage")) {
+							player.sendMessage(finalMessage);
+						}
 					}
+					plugin.log(finalMessage);
+					plugin.log("Manual command used by "
+							+ commandTyper.getName());
+				} else {
+					commandTyper
+							.sendMessage(ChatColor.DARK_RED
+									+ "Reload the configuration file to load your message!");
+					plugin.log("Reload the configuration file to load your message!");
 				}
-				plugin.log(finalMessage);
-				plugin.log("Manual command used by " + commandTyper.getName());
 			} else {
 				commandTyper
-						.sendMessage(ChatColor.DARK_RED
-								+ "Reload the configuration file to load your message!");
-				plugin.log("Reload the configuration file to load your message!");
+						.sendMessage("You don't have permission to use this command.");
 			}
-		} else {
-			commandTyper
-					.sendMessage("You don't have permission to use this command.");
+		} else if (BroadcastDonator.usingPermissions) {
+			if (BroadcastDonator.permissionHandler.has(commandTyper, "broadcastdonator.use")) {
+				if (plugin.rawMessage != null) {
+					String finalMessage = new String(
+							plugin.rawMessage.replaceAll("&([0-9a-f])",
+									"\u00A7$1"));
+					for (Player player : plugin.getServer().getOnlinePlayers()) {
+						if (!BroadcastDonator.permissionHandler.has(commandTyper, "broadcastdonator.exemptfrommessage")) {
+							player.sendMessage(finalMessage);
+						}
+					}
+					plugin.log(finalMessage);
+					plugin.log("Manual command used by "
+							+ commandTyper.getName());
+				} else {
+					commandTyper
+							.sendMessage(ChatColor.DARK_RED
+									+ "Reload the configuration file to load your message!");
+					plugin.log("Reload the configuration file to load your message!");
+				}
+			} else {
+				commandTyper
+						.sendMessage("You don't have permission to use this command.");
+			}
+		} else if (BroadcastDonator.usingOp) {
+			if (commandTyper.isOp()) {
+				if (plugin.rawMessage != null) {
+					String finalMessage = new String(
+							plugin.rawMessage.replaceAll("&([0-9a-f])",
+									"\u00A7$1"));
+					for (Player player : plugin.getServer().getOnlinePlayers()) {
+						if (!player.isOp()) {
+							player.sendMessage(finalMessage);
+						}
+					}
+					plugin.log(finalMessage);
+					plugin.log("Manual command used by "
+							+ commandTyper.getName());
+				} else {
+					commandTyper
+							.sendMessage(ChatColor.DARK_RED
+									+ "Reload the configuration file to load your message!");
+					plugin.log("Reload the configuration file to load your message!");
+				}
+			} else {
+				commandTyper
+						.sendMessage("You don't have permission to use this command.");
+			}
 		}
 	}
 
